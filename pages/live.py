6cb13685a93e0416a1c6e298a190993459ec0277
ExpiredTapWater <2302822@sit.singaporetech.edu.sg>
2025-03-20 04:44:10 +0800

    Update STUN

--- pages/live.py
+++ pages/live.py
@@ -184,6 +184,7 @@ def app_sst(status_indicator, text_output, timeout=3, energy_threshold=AMPLITUDE
             mode=WebRtcMode.SENDONLY,
             audio_receiver_size=1024,
             media_stream_constraints={"video": False, "audio": True},
+            rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
             #rtc_configuration={"iceServers": []},  # Uncomment to speed up for LOCAL use only!!
         )
 
